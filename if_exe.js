f5314091d92a48bf605280866b4762eb1a6058e3
benny_neiman <bn161299@gmail.com>
2025-11-30 16:38:47 +0200

    first commit

---
+++ if_exe.js
@@ -0,0 +1,67 @@
+// exe1
+// const score=86
+// if (score>=85) {
+
+//     console.log("Passed the test")
+// }
+// exe2
+const temperature=32
+if (temperature >30){
+    console.log("It is hot today")
+}
+// exe3
+const number =14
+if (number%2==0){
+    console.log("The number is even")
+}
+// exe4
+const balance= -5
+if (balance<0){
+    console.log("Warning: negative balance"
+)
+}
+// exe4
+// const score=120
+// if (score >100){
+//     console.log("High score bonus")
+
+
+// }
+// exe5
+const inputText=""
+if (inputText==""){
+    console.log("No input provided")
+}
+// exe6
+const age =21
+if (age>=18){
+    console.log("User is an adult"
+)
+}
+// exe7
+const speed= 95
+if (speed >90){
+console.log( "Speeding!")
+}
+// exe8
+const stock =0
+if (stock ===0){
+    console.log("Out of stock")
+}
+// exe9
+// const grade =93
+// if (grade>=90){
+//     console.log("Grade A"
+// )
+// }
+// exe10
+const score=58
+score >=60 ? console.log("pass"):console.log("fail") 
+// exe11
+const grade =60
+if (grade >=90){
+    console.log("a")}
+    else if (grade>=80){
+        console.log("b")
+    }
+else {console.log("C or below")}
